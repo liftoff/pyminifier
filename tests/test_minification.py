a2d8be2e60d7844027d2bc7e562b158d34df6660
Dan McDougall <daniel.mcdougall@liftoffsoftware.com>
2015-02-04 16:52:44 -0500

    tests/test_minification.py:  Improved the test code a bit.  You can now run it directly and it properly cleans up after itself now.

--- tests/test_minification.py
+++ tests/test_minification.py
@@ -1,15 +1,25 @@
+#!/usr/bin/env python
+
 import unittest
 import subprocess
 import tempfile
 import os
+import shutil
 
-testdir = os.path.dirname(__file__)
+testdir = os.path.abspath(os.path.split(__file__)[0])
 maindir = os.path.dirname(testdir)
 
 
 class TestPyMinify(unittest.TestCase):
+    def setUp(self):
+        self._testdir = tempfile.mkdtemp()
+        self.testdir = os.path.join(self._testdir, 'pyminifier')
+        os.mkdir(self.testdir)
+        self.sourcedir = os.path.join(maindir, 'pyminifier')
+
     def minimy_file(self, inpath, cwd):
-        proc = subprocess.Popen(['python', '-m', 'pyminifier', inpath], cwd=cwd, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(
+            ['pyminifier', inpath], cwd=cwd, stdout=subprocess.PIPE)
         data, err = proc.communicate()
         assert err is None
         return data
@@ -19,18 +29,14 @@ class TestPyMinify(unittest.TestCase):
         Test if a minified version of 'pyminifier' returns the same results as
         the original one
         """
-        _testdir = tempfile.mkdtemp()
-        testdir = os.path.join(_testdir, 'pyminifier')
-        os.mkdir(testdir)
-
-        sourcedir = os.path.join(maindir, 'pyminifier')
-
-        sourcefiles = [filename for filename in os.listdir(sourcedir) if filename.endswith('.py')]
+        sourcefiles = [
+            filename for filename
+                in os.listdir(self.sourcedir) if filename.endswith('.py')]
         results = {}
 
         for filename in sourcefiles:
-            source = os.path.join(sourcedir, filename)
-            dest = os.path.join(testdir, filename)
+            source = os.path.join(self.sourcedir, filename)
+            dest = os.path.join(self.testdir, filename)
             data = self.minimy_file(source, maindir)
             results[filename] = data
 
@@ -38,6 +44,15 @@ class TestPyMinify(unittest.TestCase):
                 destfile.write(data)
 
         for filename in sourcefiles:
-            source = os.path.join(sourcedir, filename)
-            res = self.minimy_file(source, cwd=_testdir)
+            source = os.path.join(self.sourcedir, filename)
+            res = self.minimy_file(source, cwd=self._testdir)
             self.assertEqual(res, results[filename])
+
+    def tearDown(self):
+        """
+        Clean up after ourselves
+        """
+        shutil.rmtree(self.testdir)
+
+if __name__ == '__main__':
+    unittest.main()
